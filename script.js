ac847df50347801629a0c0168d9401555d5a06e8
reiwad19 <30877953+reiwad19@users.noreply.github.com>
2017-08-14 10:32:15 -1000

    js update

---
+++ script.js
@@ -0,0 +1,7 @@
+function sayHello() {
+  //I am creating a function called sayHello//
+  var userName = prompt("Please enter your name?");
+  //Create a variable called userName and stores the isers information//
+  window.alert(userName + " Hello");
+  
+}
