a7501acd3da2b9767a1c844ba75986a5e9e22575
reiwad19 <30877953+reiwad19@users.noreply.github.com>
2017-08-16 10:23:06 -1000

    arrays
    array lesson

---
+++ arrays.js
@@ -0,0 +1,5 @@
+var juniors = ["Reimon", "Hana"];
+var seniors = ["Samiyah", "Justin", "Bryson"];
+var students = ["reimon", "hana", "will"];
+window.alert(" Juniors " + juniors.length + " Seniors " + seniors.length);
+window.alert(students[2]);
