221cc4f7ca7150084e4a4a1bafc2e106b4e8c948
reiwad19 <30877953+reiwad19@users.noreply.github.com>
2017-08-16 10:47:03 -1000

    notes
    notes

--- arrayvalues.js
+++ arrayvalues.js
@@ -2,3 +2,4 @@ var food = ["Sushi ", "Salad ", "Ramen"];
 var students = ["Brandon", "Matt", "Bryson", "Hana", "Reimon"];
 var y = food.length;
 window.alert(students[y]);
+//using .length returns an integer not a string so you can plug it in to another variable
