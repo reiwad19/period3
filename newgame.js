476452cd9eaf45315d7f71777a195ffa430f6348
reiwad19 <30877953+reiwad19@users.noreply.github.com>
2017-08-28 10:41:59 -1000

    changing game
    changing matthews favorite game

--- newgame.js
+++ newgame.js
@@ -1,9 +1,13 @@
+// made a object called matthew
+//matthew likes NBA
 var matthew = {
 	game: "NBA",
   age : 16,
   grade: 12,
 }
-
+//asked user for a new game to input
 var newGame = prompt("new game?");
+//called matthew.game and changed it to the input "newGame"
 matthew.game = newGame;
+//display new matthew.game to window
 window.alert(matthew.game);
