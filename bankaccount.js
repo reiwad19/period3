a2b9f151f0e1032ac902cf3dd1601ae4c2d3f2a2
reiwad19 <30877953+reiwad19@users.noreply.github.com>
2017-08-30 10:37:39 -1000

    bankaccount
    withdrawing from a bank account. empty object

---
+++ bankaccount.js
@@ -0,0 +1,5 @@
+var bankAccount1 = {};
+bankAccount1["name"] = "Tony";
+bankAccount1["balance"] = 1000000;
+var withdrawl = prompt("How much would you like to withdraw?");
+window.alert(bankAccount1.balance - withdrawl);
