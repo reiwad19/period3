c5ef0bf8fb21ac5f0dcb84cef6d58398f183f0f7
reiwad19 <30877953+reiwad19@users.noreply.github.com>
2017-09-06 10:51:08 -1000

    Add files via upload
    fixed syntax error

--- forloopObject.js
+++ forloopObject.js
@@ -14,8 +14,11 @@ if ((lowerCase === "classroom") || (lowerCase === "students")) {
 }
 //if the user enters anything else then "you did not pay attention", then
 //you have to try again. goes to function tryAgain
+
 else {
-  for (i=1; i<3; i++) {
+window.alert("You tried " + 1 + " times");
+
+  for (i=2; i<4; i++) {
     var x = prompt("classroom or students");
 //toLowerCase() changes all input into lowercase
 var lowerCase = x.toLowerCase();
@@ -23,7 +26,7 @@ var lowerCase = x.toLowerCase();
 if ((lowerCase === "classroom") || (lowerCase === "students")) {
 //lowerCase is the lowercased user input
   window.alert(stem2[lowerCase]);
-  i = 3;
+  i = 4;
 }
 else {
 window.alert("You tried " + i + " times");
